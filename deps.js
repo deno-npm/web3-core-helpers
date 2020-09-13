4a558b2830938b12238f8060a6ed453d9d8e7add
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-13 12:50:08 -0500

    Working implementation

---
+++ deps.js
@@ -0,0 +1,3 @@
+export * as Iban from "https://deno.land/x/npm_web3_eth_iban@0.0.1/mod.js";
+export * as utils from "https://deno.land/x/npm_web3_utils@1.2.12/mod.js";
+export * as _ from "https://cdn.skypack.dev/-/underscore@v1.11.0-8iEGGigleKa9YmBQ2bJF/dist=es2020/underscore.js";
